e64299fc0873d7751d1d52ab615bcecc35002e44
mebrahimi2 <m.ebrahimi1@email.kntu.ac.ir>
2021-12-15 17:03:01 +0330

    PEP8 requirements

--- open_data.py
+++ open_data.py
@@ -7,12 +7,11 @@ import pandas as pd
 def open_image(directory, output_type=None):
     """
     Opens all the image bands have in a directory.
-    
     The default output is a dictionary.
-    
     Args:
-        directory (string): 
-        output_type (string, optional): If you want your output to be a array, Please enter "array".
+        directory (string):
+        output_type (string, optional): If you want your output to be a array,
+        Please enter "array".
         Defaults to None.
     """
     bands = {}
@@ -31,24 +30,26 @@ def open_image(directory, output_type=None):
         for number in band_number:
             key = head + str(number)
             band_list.append(bands[key])
-        image_array = np.stack(band_list,axis=2)
+            image_array = np.stack(band_list, axis=2)
         return image_array
     return bands
 
 
-def open_csv(file_name, directory = None, output_type = None, y_header = None):
+def open_csv(file_name, directory=None, output_type=None, y_header=None):
     """
-    Opens csv file 
+    Opens csv file
 
     Args:
-        file_name (string): 
+        file_name (string):
         directory (string, optional): Defaults to None.
-        output_type ([type], optional): If you want the output array to be x and y, please enter output_type = "XY" . Defaults to None.
-        y_header (string, optional): If you selected XY output mode please enter the class header name in this variable. Defaults to None.
+        output_type ([type], optional): If you want the output array to be
+        x and y, please enter output_type = "XY" . Defaults to None.
+        y_header (string, optional): If you selected XY output mode please
+        enter the class header name in this variable. Defaults to None.
     """
     if directory:
         file_path = directory + "/" + file_name + '.csv'
-        df  = pd.read_csv(file_path)
+        df = pd.read_csv(file_path)
     if output_type:
         y = df[y_header].to_numpy()
         x = df.loc[:, df.columns != y_header].to_numpy()
